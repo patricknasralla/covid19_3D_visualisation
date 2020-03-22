2c4b262ee2915bf6c4abe8426ec891dede92566e
Patrick Nasralla <patrick.nasralla@gmail.com>
2020-03-22 12:45:23 +0000

    Initial Commit. Working react/three.js integration.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
+import * as THREE from 'three';
 import './App.css';
 
 function App() {
+  const attach = useRef();
+  const [day, setDay] = useState(0);
+
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    attach.current.appendChild( renderer.domElement );
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    camera.position.z = 5;
+    const animate = function () {
+      requestAnimationFrame( animate );
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      setDay(prevState => prevState + 0.1);
+      renderer.render( scene, camera );
+    };
+    animate();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Day: {Math.floor(day)}</p>
+    <div className="App" ref={attach}/>
+    </>
   );
 }
 
